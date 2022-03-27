c72164730ad778966a522fbd8c320355066cdd27
gustav <gustav.brolin@gmail.com>
2022-03-27 20:58:26 +0200

    Fixed WiFi connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,51 @@
 #include <Arduino.h>
+#include "WiFi.h"
+#include "../secrets.h"
+
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 4
+#endif
+
+int status = WL_IDLE_STATUS;
+void flash_led_hold();
 
 void setup()
 {
-    Serial1.begin(115200);
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.begin(115200);
+    while (status != WL_CONNECTED) 
+    {
+        Serial.print("Attempting to connect to SSID: ");
+        Serial.println(SECRET_SSID);
+        status = WiFi.begin(SECRET_SSID, SECRET_PASS);
+        delay(10000);
+    }
+
+    Serial.println("Setup done");
+    flash_led_hold();
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+    delay(5000);
+}
+
+void flash_led()
+{
+    int n = 5;
+    for (int i = 0; i < n; i++)
+    {   
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(100);
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(100);
+    }
+}
+
+void flash_led_hold()
+{
+    flash_led();
+    digitalWrite(LED_BUILTIN, HIGH);
 }
